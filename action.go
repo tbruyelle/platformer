c252ef25f77fc5f4f0ca5d39e8c83437cf2b27f9
Thomas Bruyelle <thomas.bruyelle@gmail.com>
2015-02-05 17:51:09 +0100

    Player stop but very rude way to do

--- action.go
+++ action.go
@@ -3,12 +3,11 @@ package main
 import (
 	"github.com/tbruyelle/fsm"
 	"golang.org/x/mobile/sprite/clock"
-	"math"
 )
 
 type moveTo struct {
-	x, y   float32
-	vx, vy float32
+	x, y float32
+	v    *fsm.Vector
 }
 
 const (
@@ -18,17 +17,17 @@ const (
 func (a *moveTo) Do(o *fsm.Object, t clock.Time) {
 	if o.Time == 0 {
 		o.Time = t
+		// Fix x,y to match the player dimension
+		a.x, a.y = a.x-o.Width/2, a.y-o.Height/2
 		// Compute vector to direction
-		a.vx = a.x - o.Width/2 - o.X
-		a.vy = a.y - o.Width/2 - o.Y
-		// Compute vector length
-		l := float32(math.Sqrt(float64(a.vx*a.vx + a.vy*a.vy)))
+		a.v = fsm.NewVector(o.X, o.Y, a.x, a.y)
 		// Normalize vector
-		a.vx, a.vy = a.vx/l, a.vy/l
+		a.v.Normalize()
 	}
 	f := clock.EaseIn(o.Time, o.Time+10, t)
-	o.Vx, o.Vy = a.vx*playerSpeed*f, a.vy*playerSpeed*f
-	if o.X == a.x && o.Y == a.y {
+	o.Vx, o.Vy = a.v.X*playerSpeed*f, a.v.Y*playerSpeed*f
+	// Check if the goal is reached
+	if fsm.NewVector(o.X, o.Y, a.x, a.y).Length() <= playerSpeed {
 		// move over
 		o.Reset()
 		o.Action = nil
